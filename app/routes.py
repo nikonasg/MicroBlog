5237a6dd66475b02bd16b2a9aa0e0463501149df
Nik Gianitsos <gianitsos04@icloud.com>
2018-05-23 19:16:59 -0500

    Jinja templates

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World!"
+	user = {'username': 'Nik'}
+	post = { 'author': {'username': 'Jake'},'body': 'Infinity War was awesome! 10/10'}
+	return render_template('index.html', title='Home', user=user, post=post)
 
-@app.route('/poop')
-def poop():
-	return "<p>Poop<p>"
+
+
+	#{% if text %} is a conditional statement that works like this:
+	#{% if title %}
+	#<title>{{ title }} - Welcome!</title>
+	#{% else %}
+	#<title>Home, Welcome!</title>
+	#{% endif %}
