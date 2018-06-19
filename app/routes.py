8c2c54bb3fded11a5a5d54c533057249eef84b55
Nik Gianitsos <gianitsos04@icloud.com>
2018-06-18 19:36:32 -0500

    Login page

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -7,6 +8,15 @@ def index():
 	user = {'username': 'Nik'}
 	post = { 'author': {'username': 'Jake'},'body': 'Infinity War was awesome! 10/10'}
 	return render_template('index.html', title='Home', user=user, post=post)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for user {}, Remember_me={}'.format(
+			form.username.data, form.Remember_me.data))
+		return redirect('/index')
+	return render_template('login.html', title='Sign In', form=form)
 
 
 
